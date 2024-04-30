a9bcad673ed8ec1a480f62c2c17ea669a4e6ab7d
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2024-04-30 13:26:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -64,8 +64,8 @@ class Instrument {
 } // you figure it out imma et pasta now brb go eat you said that 4 times
 
 
-var voice = new Instrument(['./assets/voice/a3.wav', './assets/voice/a4.wav', './assets/voice/a5.wav']);
-var electricpiano = new Instrument(['./assets/electricpiano/a2.wav', './assets/electricpiano/a3.wav', './assets/electricpiano/a4.wav', './assets/electricpiano/a5.wav', './assets/electricpiano/a6.wav', './assets/electricpiano/a7.wav']);
+var voice = new Instrument(['assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
+var electricpiano = new Instrument(['assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
 
 async function load() {
   await voice.load();
