7ac56a12463ceb9e80e0931131fa093a0b34b7d6
michaelcup405 <88284261+michaelcup405@users.noreply.github.com>
2024-04-30 16:30:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -64,15 +64,19 @@ class Instrument {
 } // you figure it out imma et pasta now brb go eat you said that 4 times
 
 
-var voice = new Instrument(['', '', '', 'assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
-var electricpiano = new Instrument(['', '', 'assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
+var voice = new Instrument(['assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
+var electricpiano = new Instrument(['assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
+var harpsichord = new Instrument(['assets/harpsichord/a2.wav', 'assets/harpsichord/a3.wav', 'assets/harpsichord/a4.wav', 'assets/harpsichord/a5.wav', 'assets/harpsichord/a6.wav']);
+var grandpiano = new Instrument(['assets/grandpiano/a2.wav', 'assets/grandpiano/a3.wav', 'assets/grandpiano/a4.wav', 'assets/grandpiano/a5.wav', 'assets/grandpiano/a6.wav', 'assets/grandpiano/a7.wav']);
 
 async function load() {
   await voice.load();
   await electricpiano.load();
+  await harpsichord.load();
+  await grandpiano.load();
   console.log('loaded');
 }
-// imma eat pasta brb 10-15 minutes
+
 load();
 
 const note = document.querySelector('#note');
@@ -85,11 +89,8 @@ setInterval(() => {
   return noteInput;
 }  , 1);
 
-/*
-var mousex = 0;
-var mousey = 0;
-
-var mousedown = false;
-
-output.onmousemove = (e) => {};
-*/
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
+    electricpiano.playNote('G3');
+  }
+});
