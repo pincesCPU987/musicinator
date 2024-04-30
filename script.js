1566736546ee55f90d03844a547ac88928a23bc7
michaelcup405 <88284261+michaelcup405@users.noreply.github.com>
2024-04-30 21:53:56 +0000

    Les go, cool changes!

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// Buttons
+const editButton = document.getElementById("editButton");
+const selectButton = document.getElementById("selectButton");
+const eraserButton = document.getElementById("eraserButton");
+const selectAllButton = document.getElementById("selectAllButton");
+const selectTypeButton = document.getElementById("selectTypeButton");
+const saveButton = document.getElementById("saveButton");
+
+// canvas 
 var output = document.querySelector("#output");
 var ctx = output.getContext("2d");
 
 
-
+// audio context
 var audioCtx = new AudioContext();
 
 var audioPlay = async (url) => {
@@ -19,6 +28,7 @@ var audioPlay = async (url) => {
 	src.start();
 };
 
+//instrument class (duh)
 class Instrument {
 	constructor(assets) {
 		this.assets = assets;
@@ -63,12 +73,14 @@ class Instrument {
   }
 } // you figure it out imma et pasta now brb go eat you said that 4 times
 
-
+// Instruments and their sources
 var voice = new Instrument(['assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
 var electricpiano = new Instrument(['assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
 var harpsichord = new Instrument(['assets/harpsichord/a2.wav', 'assets/harpsichord/a3.wav', 'assets/harpsichord/a4.wav', 'assets/harpsichord/a5.wav', 'assets/harpsichord/a6.wav']);
 var grandpiano = new Instrument(['assets/grandpiano/a2.wav', 'assets/grandpiano/a3.wav', 'assets/grandpiano/a4.wav', 'assets/grandpiano/a5.wav', 'assets/grandpiano/a6.wav', 'assets/grandpiano/a7.wav']);
 
+
+//load the instruments
 async function load() {
   await voice.load();
   await electricpiano.load();
@@ -79,6 +91,7 @@ async function load() {
 
 load();
 
+//test note updater
 const note = document.querySelector('#note');
 const input = document.querySelector('#noteInput');
 
@@ -94,3 +107,4 @@ document.addEventListener("keydown", function (event) {
     electricpiano.playNote('G3');
   }
 });
+//end test note updater
