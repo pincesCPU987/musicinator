9b9444bb925b0975f6147fda9120d428ee32c454
michaelcup405 <88284261+michaelcup405@users.noreply.github.com>
2024-05-01 14:18:25 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 canvas = document.querySelector('#output');
 const ctx = canvas.getContext('2d');
-
+/*
 // Draw white keys
 ctx.fillStyle = 'white';
 for (let i = 0; i < 14; i++) {
@@ -12,3 +12,4 @@ ctx.fillStyle = 'black';
 for (let i = 1; i < 14; i += 2) {
   ctx.fillRect(i * 30, 0, 20, 120);
 }
+*/
