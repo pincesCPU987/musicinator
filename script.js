37c3287b880ac543152869c038d3d3c69da7f895
michaelcup405 <88284261+michaelcup405@users.noreply.github.com>
2024-05-01 10:56:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const qwertyLayout = [
 
 // Define the corresponding piano notes
 const qwertyNotes = [
-  ['C5', 'D5', 'E5', 'F5', 'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F6', 'G6'],
-  ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5'],
-  ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4']
+  ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5'],
+  ['C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F4'],
+  ['C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3']
 ];
 
 
@@ -83,17 +83,17 @@ class Instrument {
     var source = audioCtx.createBufferSource();
     source.buffer = sample;
     source.detune.value = detune; // man i love and hate this ghostwriter
-    
+
     source.connect(audioCtx.destination);
     source.start(); // duh
-  }
-} // you figure it out imma et pasta now brb go eat you said that 4 times
+  } // you figure it out imma et pasta now brb go eat you said that 4 times
 
+}
 // Instruments and their sources
-var voice = new Instrument(['assets/nothing.wav', 'assets/nothing.wav', 'assets/nothing.wav', 'assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
-var electricpiano = new Instrument(['assets/nothing.wav', 'assets/nothing.wav', 'assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
-var harpsichord = new Instrument(['assets/nothing.wav', 'assets/nothing.wav', 'assets/harpsichord/a2.wav', 'assets/harpsichord/a3.wav', 'assets/harpsichord/a4.wav', 'assets/harpsichord/a5.wav', 'assets/harpsichord/a6.wav']);
-var grandpiano = new Instrument(['assets/nothing.wav', 'assets/nothing.wav', 'assets/grandpiano/a2.wav', 'assets/grandpiano/a3.wav', 'assets/grandpiano/a4.wav', 'assets/grandpiano/a5.wav', 'assets/grandpiano/a6.wav', 'assets/grandpiano/a7.wav']);
+var voice = new Instrument(['assets/voice/a3.wav', 'assets/voice/a3.wav', 'assets/voice/a3.wav', 'assets/voice/a3.wav', 'assets/voice/a3.wav', 'assets/voice/a4.wav', 'assets/voice/a5.wav']);
+var electricpiano = new Instrument(['assets/electricpiano/a2.wav', 'assets/electricpiano/a3.wav', 'assets/electricpiano/a4.wav', 'assets/electricpiano/a5.wav', 'assets/electricpiano/a6.wav', 'assets/electricpiano/a7.wav']);
+var harpsichord = new Instrument(['assets/harpsichord/a2.wav', 'assets/harpsichord/a2.wav', 'assets/harpsichord/a3.wav', 'assets/harpsichord/a4.wav', 'assets/harpsichord/a5.wav', 'assets/harpsichord/a6.wav']);
+var grandpiano = new Instrument(['assets/grandpiano/a2.wav', 'assets/grandpiano/a3.wav', 'assets/grandpiano/a4.wav', 'assets/grandpiano/a5.wav', 'assets/grandpiano/a6.wav', 'assets/grandpiano/a7.wav']);
 
 
 //load the instruments
@@ -108,6 +108,7 @@ async function load() {
 load();
 
 //test note updater
+// initialize vars
 const note = document.querySelector('#note');
 const input = document.querySelector('#noteInput');
 const instrumentSelector = document.getElementById('Instrument');
@@ -162,6 +163,5 @@ document.addEventListener('keydown', (event) => {
   if (row !== -1 && col !== -1) {
     const note = qwertyNotes[row][col];
     playDetectedInstrument(note);
-    // You can perform any other action with the note here
   }
 });
