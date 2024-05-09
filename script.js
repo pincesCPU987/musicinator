e85171858fe8f72aec9f65239aca51579f61ba0f
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2024-05-08 19:34:11 -0700

    Found that stupid problem

--- script.js
+++ script.js
@@ -255,9 +255,9 @@ function onNote(note, velocity) {
   console.log(note, velocity);
   if (velocity > 0) {
     midiActive[note] = 1;
-    var sequence = ('C,C#,D,D#,E,F,F#,G,G#,A,A#,B').split(',');
-    var octave = Math.floor(note / 12) + 1;
-    note = sequence[note % 12] + octave;
+    //var sequence = ('C,C#,D,D#,E,F,F#,G,G#,A,A#,B').split(',');
+    //var octave = Math.floor(note / 12) + 1;
+    //note = sequence[note % 12] + octave;
     if (selectedInstrument === 'electricpiano') {
       electricpiano.playNote(note, 'key', {note:note}, true);
     } if (selectedInstrument === 'grandpiano') {
